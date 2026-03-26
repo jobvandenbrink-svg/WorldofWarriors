e696c6f52ed63fe0479bf237b6c8f2347c4e269e
jobvandenbrink <jobvandenbrink@gmail.com>
2026-03-26 09:28:44 +0100

    chhanfes

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // Enable HTTP on port 80
+    options.ListenAnyIP(80);
+
+    // Enable HTTPS on port 443 (Railway terminates TLS anyway)
+    options.ListenAnyIP(443, listenOptions =>
+    {
+        listenOptions.UseHttps();
+    });
+});
+
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 app.Urls.Add($"http://0.0.0.0:{port}");
 
