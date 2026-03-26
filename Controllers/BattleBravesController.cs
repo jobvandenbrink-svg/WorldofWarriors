e9a23a0d8cf4a667a0e9314099eace10eb7aa19a
jobvandenbrink <jobvandenbrink@gmail.com>
2026-03-26 10:23:08 +0100

    changes 4

--- Controllers/BattleBravesController.cs
+++ Controllers/BattleBravesController.cs
@@ -1,24 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
+var builder = WebApplication.CreateBuilder(args);
 
-[ApiController]
-[Route("battle-braves")]
-public class BattleBravesController : ControllerBase
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+builder.WebHost.ConfigureKestrel(options =>
 {
-    public class LoginData
-    {
-        public string AUID { get; set; }
-    }
+    options.ListenAnyIP(int.Parse(port));
+});
 
-    [HttpPost("login")]
-    public IActionResult Login([FromBody] LoginData data)
-    {
-        if (string.IsNullOrEmpty(data?.AUID))
-            return BadRequest(new { error = "Missing AUID" });
+builder.Services.AddControllers();
 
-        return Ok(new {
-            sessionToken = "TOKEN-" + Guid.NewGuid().ToString("N"),
-            gameUserID = "USER-" + Guid.NewGuid().ToString("N"),
-            deviceTokenHash = "hash123"
-        });
-    }
-}
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
